5102ebdd19d692e21da44d5fb7f923385c2eae28
Eric Liu <dathtohn@gmail.com>
2013-04-24 16:18:25 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ajisen2::Application.config.secret_token = 'bfc037c498ce33fe6564671fae7d8d902884fe132f28eae0df4ae967ac8a74f0e10c675a7a9b48cafadc878e61a1346cce3bb8e9f413b114572a16ad0a4e4b2a'
