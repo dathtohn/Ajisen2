698378f3f791248ea44105a59ad4bc3f8ed4efd3
Eric Liu <dathtohn@gmail.com>
2013-04-27 10:02:45 -0400

    Finish user edit, update, index, pagination, and destroy with admin actions

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Ajisen2::Application.routes.draw do
   root to: 'static_pages#home'
   
   match '/signup',    to: 'users#new'
+  #match '/edit',      to: 'users#edit'
   match '/signin',    to: 'sessions#new'
   match '/signout',   to: 'sessions#destroy', via: :delete
   
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Eric Liu"
-    email    "eric@ajisen.com"
-    password "ericliu"
-    password_confirmation "ericliu"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
